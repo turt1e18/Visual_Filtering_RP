8c9342f6cf9d58fb1acd4d7e4c81b93dbf81efff
test <pk0820@cu.ac.kr>
2024-05-21 19:57:16 +0900

    :feat2 code + main code 19:57

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-from streaming.app import main as s_main
+# from streaming.app import main as s_main
+# import os
+# import subprocess
+
+
+# path = os.getcwd()
+# ui_path = os.path.join(path, "ui", "ui_program.py")
+
+# ui_process = subprocess.Popen(["python", ui_path], cwd=os.path.dirname(ui_path))
+# ui_process.wait()
+
+# s_main()
+
 import os
 import subprocess
+from streaming.app import main as s_main
 
+def run_ui_and_main():
+    # 현재 작업 디렉토리를 가져옴
+    path = os.getcwd()
 
-path = os.getcwd()
-ui_path = os.path.join(path, "ui", "ui_program.py")
+    # UI 파일의 경로 설정
+    ui_path = os.path.join(path, "ui", "ui_program.py")
 
-ui_process = subprocess.Popen(["python", ui_path], cwd=os.path.dirname(ui_path))
-ui_process.wait()
+    # UI 파일 실행
+    ui_process = subprocess.Popen(["python", ui_path], cwd=os.path.dirname(ui_path))
+    ui_process.wait()
 
-s_main()
+    # s_main 함수 실행
+    s_main()
+
+if __name__ == "__main__":
+    run_ui_and_main()
